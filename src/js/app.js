d50d73c99256d4ac977edc0f4fb2792f6d6045dd
Andy Preston <edgeeffect@gmail.com>
2020-11-10 16:36:46 +0000

    added vue-xyscope to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import { BootstrapVue/*, IconsPlugin*/ } from 'bootstrap-vue';
+import '../../node_modules/xyscope/src/vue-xyscope';
 import '../sass/app.scss';
 
 Vue.use(BootstrapVue);
