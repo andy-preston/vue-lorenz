5920f7243b49dc39da09e76bde0232f5b7f5ccb6
Andy Preston <edgeeffect@gmail.com>
2020-11-10 16:37:06 +0000

    added Vue App code to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
 import { BootstrapVue/*, IconsPlugin*/ } from 'bootstrap-vue';
+import App from '../App.vue';
 import '../../node_modules/xyscope/src/vue-xyscope';
 import '../sass/app.scss';
 
 Vue.use(BootstrapVue);
 //Vue.use(IconsPlugin);
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
