edaf768a4dab1b3c58046a4a72efa170f8991b44
Andy Preston <edgeeffect@gmail.com>
2020-11-10 16:36:22 +0000

    changes missed out in 0971739fb58098def05e3185ec6d117c52e1c402

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import { BootstrapVue/*, IconsPlugin*/ } from 'bootstrap-vue';
-import '../sass/index.scss';
+import '../sass/app.scss';
 
 Vue.use(BootstrapVue);
 //Vue.use(IconsPlugin);
