0658ed9ff8394ccc4f061f961c6c23ee37156429
Andy Preston <edgeeffect@gmail.com>
2020-11-10 16:34:24 +0000

    Add Vue application "component"

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <b-container>
+            <b-button-toolbar>
+                <b-button>Edit</b-button>
+                <b-button>Undo</b-button>
+                <b-button>Redo</b-button>
+            </b-button-toolbar>
+            <xyscope
+                ref="scope"
+                limits="-40, -5, 40, 60"
+                v-on:request-data="requestData"
+            ></xyscope>
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss">
+#app {
+    font-family: sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin-top: 20px;
+}
+</style>
