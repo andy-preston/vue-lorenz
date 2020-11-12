b9ff6e6dc12e955b37397f2a72ede6092d7fdbc3
Andy Preston <edgeeffect@gmail.com>
2020-11-12 14:41:44 +0000

    Added adjust box to modify "rho"

---
+++ src/vue/AdjustBox.vue
@@ -0,0 +1,44 @@
+<template>
+    <b-form-group>
+        <label for="input">{{ label }}: {{ value }}</label>
+        <b-form-input
+            id="input"
+            v-model="value"
+            type="range"
+            :min="min"
+            :max="max"
+        >
+        </b-form-input>
+    </b-form-group>
+</template>
+
+<script>
+export default {
+    'props': {
+        'id': {
+            'type': String,
+            'required': true
+        },
+        'label': {
+            'type': String,
+            'required': true
+        },
+        'min': {
+            'type': Number,
+            'required': true
+        },
+        'max': {
+            'type': Number,
+            'required': true
+        }
+    },
+    data() {
+        return {
+            'value': 0
+        };
+    }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/vue/App.vue
+++ src/vue/App.vue
@@ -2,18 +2,28 @@
     <div id="app">
         <b-container>
             <b-button-toolbar id="top-bar">
+                <AdjustBox
+                    id="rhoBox"
+                    label="rho"
+                    min="14"
+                    max="28"
+                ></AdjustBox>
+
                 <StartStopBtn
                     @start="startScope"
                     @stop="stopScope"
                 ></StartStopBtn>
+
                 <VariableBox
                     id="xBox"
                     label="x"
                 ></VariableBox>
+
                 <VariableBox
                     id="yBox"
                     label="y"
                 ></VariableBox>
+
                 <VariableBox
                     id="zBox"
                     label="z"
@@ -31,13 +41,11 @@
 <script>
 import StartStopBtn from './StartStopBtn.vue';
 import VariableBox from './VariableBox.vue';
+import AdjustBox from './AdjustBox.vue';
 
 export default {
     name: 'App',
-    components: {
-        StartStopBtn,
-        VariableBox
-    },
+    components: { StartStopBtn, VariableBox, AdjustBox },
     'methods': {
         'startScope': function () {
         },
