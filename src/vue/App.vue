96894e52e3cddf28fbfd838ac1b1a5ff172dca7c
Andy Preston <edgeeffect@gmail.com>
2020-11-17 18:17:25 +0000

    Handling start and stop

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -2,6 +2,8 @@
     <div id="app">
         <Toolbar
             ref="toolbar"
+            @start="startScope"
+            @stop="stopScope"
             @rho-change="rhoChange"
         ></Toolbar>
         <TheScope
@@ -14,16 +16,31 @@
 <script>
 import Toolbar from './Toolbar.vue';
 import TheScope from './TheScope.vue';
+var running = false;
+var waitingForData = false;
 
 export default {
     name: 'App',
     components: { Toolbar, TheScope },
     'methods': {
+        'getData': function () {
+            if (running && waitingForData) {
+                waitingForData = false;
+            }
+        },
+        'startScope': function () {
+            running = true;
+            this.getData();
+        },
+        'stopScope': function () {
+            running = false;
+        },
         'rhoChange': function (newValue) {
             lorenz.rho(newValue);
         },
         'requestData': function () {
-            console.log('request-data');
+            waitingForData = true;
+            this.getData();
         }
     }
 };
