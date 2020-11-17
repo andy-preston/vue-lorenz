8ca312b347aa8111fcaa8de5932cbfa10e617976
Andy Preston <edgeeffect@gmail.com>
2020-11-17 18:17:56 +0000

    Actually getting some values and doing something with them

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -16,6 +16,9 @@
 <script>
 import Toolbar from './Toolbar.vue';
 import TheScope from './TheScope.vue';
+import Lorenz from '../js/lorenz';
+
+const lorenz = Lorenz();
 var running = false;
 var waitingForData = false;
 
@@ -26,6 +29,9 @@ export default {
         'getData': function () {
             if (running && waitingForData) {
                 waitingForData = false;
+                let nextValue = lorenz.nextValue();
+                this.$refs.toolbar.nextValue = nextValue;
+                this.$refs.scope.nextValue(nextValue);
             }
         },
         'startScope': function () {
