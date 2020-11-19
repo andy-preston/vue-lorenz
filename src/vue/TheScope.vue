476f164c2933bff88b2deb7a4b2bb4608793e233
Andy Preston <edgeeffect@gmail.com>
2020-11-19 16:47:42 +0000

    Handle resizing

--- src/vue/TheScope.vue
+++ src/vue/TheScope.vue
@@ -14,9 +14,23 @@
 
 <script>
 export default {
+    'mounted': function () {
+        window.addEventListener('resize', this.onResize);
+        this.onResize();
+    },
     'methods': {
         'nextValue': function (nextValue) {
             this.$refs.scope.pushData({ 'x': nextValue.x, 'y': nextValue.z });
+        },
+        'onResize': function () {
+            // Force the scope canvas to fill the ramaining screen under the
+            // toolbar
+            const theCanvas = document.getElementById('canvas');
+            theCanvas.style.height = (
+                window.innerHeight - 16 -
+                theCanvas.getBoundingClientRect().top
+            ) + 'px';
+            this.$refs.scope.rescale();
         }
     }
 };
